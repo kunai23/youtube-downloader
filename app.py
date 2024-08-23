76a6262f9b0037f89d095e35a8c9829d987c9745
kunai23 <111249708+kunai23@users.noreply.github.com>
2024-08-23 21:41:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, flash
 from pytube import YouTube
 import os
 import psutil
 import secrets
+import yt_dlp
 
 app = Flask(__name__)
 
@@ -27,6 +28,20 @@ def index():
 @app.route('/completed')
 def completed():
     return render_template('completed.html')
+
+def download_youtube_video(url, save_path):
+    try:
+        ydl_opts = {
+            'format': 'bestvideo[ext=mp4][height<=1440]+bestaudio[ext=m4a]/best[ext=mp4][height<=1440]',
+            'outtmpl': os.path.join(save_path, '%(title)s.%(ext)s'),
+            'merge_output_format': 'mp4',
+            'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.extract_info(url, download=True)
+    except Exception as e:
+        print(f'An error occurred: {e}')
+        raise
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
