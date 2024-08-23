a65a17d31e4b8655f2e6d4d7cd7b844e2412c0f7
kunai23 <111249708+kunai23@users.noreply.github.com>
2024-08-23 19:49:13 +0200

    Update2 app.py

--- app.py
+++ app.py
@@ -44,5 +44,6 @@ def download_youtube_video(url, save_path):
         print(f'An error occurred: {e}')
         raise
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
